f192233363aae2f455a1446e7e8993e8cae17f03
chalanaK95 <ChalanaK@fortude.co>
2022-08-02 12:15:12 +0530

    client add mutation done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Clients from "./components/Clients";
+import AddClientModal from "./components/AddClientModal";
 
 
 const client = new ApolloClient({
@@ -14,6 +15,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
